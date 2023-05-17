6a917da4c655a9297dfa23a576827a5b40f6dac6
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-05-16 20:13:00 -0600

    Add basic word-storing functionality

--- script.js
+++ script.js
@@ -10,10 +10,21 @@ statistics after each win/loss
 let wordList = ['truck', 'elect'];
 let prototypeWord = 'truck';
 
-const squareList = document.querySelectorAll('.square');
+const squareList = Array.from(document.querySelectorAll('.square'));
 
 let counter = 0;
+let word = "";
 document.addEventListener('keydown', function(event) {
     squareList[counter].textContent = event.key;
     counter++;
+    if (counter % 5 === 0 && counter !== 0) {
+        squareList.slice(counter - 5, counter + 1).forEach(function(square) {
+            word+= square.textContent;
+            if (word.length === 5) {
+                console.log(word);
+                word = "";
+            }
+        });
+    }
 })
+
